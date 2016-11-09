ecbac29a3bebb6bab95d2a02b2c07bda947c8621
Bhargav Gurlanka <bhargav.grlnk@gmail.com>
2016-11-09 23:44:55 +0700

    Remove the unimplemented tests

--- C/tests/main.c
+++ C/tests/main.c
@@ -1,8 +1,6 @@
 #include "libs/minunit.h"
-#include "args-parser-extract-arguments-test-suite.h"
 
 int main(int argc, char *argv[]) {
-    MU_RUN_SUITE(arg_parser_extract_arguments_test_suite);
-    MU_REPORT();
+    //FIXME: add tests
     return 0;
 }
